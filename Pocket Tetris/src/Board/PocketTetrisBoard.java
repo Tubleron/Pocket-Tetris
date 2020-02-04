da4ae2d41340137a0f42bc9bb67e7bee6adbfdeb
Иван Шопов <60072026+Tubleron@users.noreply.github.com>
2020-02-04 02:58:59 +0200

    Add files via upload

---
+++ Pocket Tetris/src/Board/PocketTetrisBoard.java
@@ -0,0 +1,8 @@
+package Board;
+
+import Figures.Figure;
+
+public class PocketTetrisBoard {
+
+	public static Figure board[][] = new Figure[24][12];
+}
---
+++ Pocket Tetris/src/Figures/Color/FiguresColor.java
@@ -0,0 +1,14 @@
+package Figures.Color;
+
+public class FiguresColor {
+
+	    public final static String YELLOW = "#FFFF33";
+
+		public final static String GREEN = "#009900";
+		
+		public final static String BRAWN = "#663300";
+
+		public final static String GRAY = "#C0C0C0";
+		
+		public final static String PINK = "#FF9999";
+	}
